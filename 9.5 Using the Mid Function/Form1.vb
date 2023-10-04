2278a2d4caf9056953c7c4449a01b921fad09c90
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-04 19:59:22 +0200

    Add project files.

---
+++ 9.5 Using the Mid Function/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+	Private Sub BtnShow_Click(sender As Object, e As EventArgs) Handles BtnShow.Click
+		Dim myPhrase As String
+		Dim LblPhrase
+		Dim LblExtract
+		myPhrase = InputBox("Enter your phrase")
+
+		LblPhrase = myPhrase
+
+		LblExtract = MsgBox(Mid(myPhrase, 2, 6)) ' the starting position is 2 , then we extract 6 characters iclusing the space
+
+	End Sub
+End Class
